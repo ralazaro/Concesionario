aad10bf04f53e0c6363b56bd7f12bff5419501ad
ralazaro <rules166@hotmail.com>
2016-02-05 20:06:24 +0100

    formulario de alta de un cliente

---
+++ Formulario/ClienteNuevo.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Formulario
+{
+    partial class ClienteAlta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.nom = new System.Windows.Forms.TextBox();
+            this.ape = new System.Windows.Forms.TextBox();
+            this.tel = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(93, 61);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Nombre";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(93, 104);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 13);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Apellidos";
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(93, 151);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(77, 13);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Teléfono";
+            // 
+            // nom
+            // 
+            this.nom.Location = new System.Drawing.Point(177, 58);
+            this.nom.Name = "nom";
+            this.nom.Size = new System.Drawing.Size(172, 20);
+            this.nom.TabIndex = 1;
+            // 
+            // ape
+            // 
+            this.ape.Location = new System.Drawing.Point(177, 102);
+            this.ape.Name = "ape";
+            this.ape.Size = new System.Drawing.Size(172, 20);
+            this.ape.TabIndex = 1;
+            // 
+            // tel
+            // 
+            this.tel.Location = new System.Drawing.Point(177, 148);
+            this.tel.Name = "tel";
+            this.tel.Size = new System.Drawing.Size(172, 20);
+            this.tel.TabIndex = 1;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(82, 212);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 2;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(238, 212);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 2;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // ClienteAlta
+            // 
+            this.ClientSize = new System.Drawing.Size(372, 280);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.tel);
+            this.Controls.Add(this.ape);
+            this.Controls.Add(this.nom);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Name = "ClienteAlta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;    
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox nom;
+        private System.Windows.Forms.TextBox ape;
+        private System.Windows.Forms.TextBox tel;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
